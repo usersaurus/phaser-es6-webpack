4d5cba968bfef01bf0298455e83f0aae90332e5f
Antonio Roman <antluisroman@gmail.com>
2018-08-07 22:30:16 +0200

    replaced asset. es-lint pending

--- src/scenes/simple-scene.js
+++ src/scenes/simple-scene.js
@@ -1,10 +1,10 @@
 export class SimpleScene extends Phaser.Scene {
   preload() {
-    this.load.image("cokecan", "assets/ash.gif");
+    this.load.image("invader", "assets/invader.png");
   }
 
   create() {
     this.add.text(100, 100, "Hello Phaser!", { fill: "#0f0" });
-    this.add.image(100, 200, "cokecan");
+    this.add.image(100, 200, "invader");
   }
 }
